4d7918fa1fa433d29e0cc11b6421c980ebb1bd51
Timo Varis <383338+timovaris@users.noreply.github.com>
2024-01-30 23:11:54 +0200

    Lisätty offline-kello
    Lisätty sivulle analoginen kello P:n kellonaikojen opiskelua varten.

---
+++ analog-clock/service-worker.js
@@ -0,0 +1,28 @@
+// Tämä on esimerkki palvelutyöntekijästä
+const CACHE_NAME = 'v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/kello.png',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
